0f310a3441c40a6163eaa5b76e165eba46d68adb
Teppo <teppo@protonmail.com>
2023-09-13 18:19:42 +0300

    Updated NHIS example spec to v. 0.3

--- tests/testthat/test-read.R
+++ tests/testthat/test-read.R
@@ -1,13 +1,13 @@
-# library( validate )
-# library( tidyverse )
-# library( readxl )
+library( validate )
+library( tidyverse )
+library( readxl )
 # library( testthat )
 
 
 # Read Excel specification tables
 excel_spec_table_list <- td_read_excel_to_list(
     system.file(
-        'stadas_v-0-1_nhis.xlsx',
+        'nhis_stadas-v-0-3.xlsx',
         package = 'rstadas',
         mustWork = TRUE
     )
@@ -18,6 +18,7 @@ specs <- td_create_specs(
     excel_spec_table_list,
     validation_summary = TRUE
 )
+
 names( specs )
 
 
@@ -36,16 +37,22 @@ names( specs )
 # )
 
 
-df.2021 <- td_read_file(
+df.2021 <- td_read_data_file(
     file_path = file.path( '.', 'nhis', 'nhis_2021.zip' ),
     read_func = read_csv,
     specs = specs,
 
+    drop_na_colnames = NULL,  # default
+
+    varmap_func = td_varmap,  # default
+
+    mapping_key = NULL,  # default
     # Pick a 4-digit key from the file name for mapping names
     mapping_key_pattern = '[0-9]{4}',
 
     # Use rstadas function to add the survey year as metadata
     metadata_func = td_metadata_year,
+    metadata_df = NULL,  # default
 
     # Drop original (raw) data columns
     keep_originals = FALSE,
@@ -56,15 +63,67 @@ df.2021 <- td_read_file(
 )
 
 
-df <- td_read_dir(
+df <- td_read_data_dir(
     dir_path = file.path( '.', 'nhis' ),
     specs = specs,
     read_func = read_csv,
+
+    drop_na_colnames = NULL,  # default
+
+    varmap_func = td_varmap,  # default
+
+    mapping_key = NULL,  # default
     mapping_key_pattern ='[0-9]{4}',
+
     metadata_func = td_metadata_year,
+    metadata_df = NULL,  # default
+
     keep_originals = FALSE,
 
     # Read all as text
     col_types = cols( .default = col_character() )
 )
 
+
+
+# test_that(
+#     'Data conforms to the specs', {
+#         expect_named(
+#             df.2021,
+#             c(
+#                 'age', 'sex', 'height', 'weight', 'edulevel',
+#                 'nof_adults', 'nof_children', 'maritalstatus',
+#                 'health', 'lifesatisfaction',
+#                 'diff_walk100yd', 'diff_walk033miles', 'diff_walksteps',
+#                 'diff_communication', 'diff_remconc', 'diff_errands',
+#                 'diff_socactivities',
+#                 'mh_anxiety', 'mh_depression',
+#                 'k6_sadness', 'k6_nervous', 'k6_restless',
+#                 'k6_hopeless', 'k6_effort', 'k6_worthless'
+#             )
+#         )
+#     }
+# )
+#
+#
+#
+# test_that(
+#     'Data conforms to the specs', {
+#         expect_named(
+#             df,
+#             c(
+#                 'age', 'sex', 'height', 'weight', 'edulevel',
+#                 'nof_adults', 'nof_children', 'maritalstatus',
+#                 'health', 'lifesatisfaction',
+#                 'diff_walk100yd', 'diff_walk033miles', 'diff_walksteps',
+#                 'diff_communication', 'diff_remconc', 'diff_errands',
+#                 'diff_socactivities',
+#                 'mh_anxiety', 'mh_depression',
+#                 'k6_sadness', 'k6_nervous', 'k6_restless',
+#                 'k6_hopeless', 'k6_effort', 'k6_worthless'
+#             )
+#         )
+#     }
+# )
+#
+#
--- tests/testthat/test-specs.R
+++ tests/testthat/test-specs.R
@@ -1,22 +1,25 @@
-# library( validate )
+library( validate )
 # library( data.table )
-# library( tidyverse )
+library( tidyverse )
 # library( readxl )
-# library( testthat )
+library( testthat )
 # library( usethis )
 
 
 # Read Excel specification tables
 excel_spec_table_list <- td_read_excel_to_list(
     system.file(
-        'stadas_v-0-1_nhis.xlsx',
+        'nhis_stadas-v-0-3.xlsx',
         package = 'rstadas',
         mustWork = TRUE
     )
 )
 
 # Turn the list into a standardized spec
-specs <- td_create_specs( excel_spec_table_list )
+specs <- td_create_specs(
+    excel_spec_table_list,
+    validation_summary = TRUE
+)
 
 
 test_that(
@@ -27,98 +30,11 @@ test_that(
                 'setup',
                 'variables',
                 'categories',
+                'colors',
                 'rules',
                 'na'
             )
         )
     }
 )
-
-
-# Download NHIS datasets
-# download.file(
-#     url = 'https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/NHIS/2019/adult19csv.zip',
-#     destfile = file.path( '.', 'nhis', 'nhis_2019.zip' )
-# )
-# download.file(
-#     url = 'https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/NHIS/2020/adult20csv.zip',
-#     destfile = file.path( '.', 'nhis', 'nhis_2020.zip' )
-# )
-# download.file(
-#     url = 'https://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/NHIS/2021/adult21csv.zip',
-#     destfile = file.path( '.', 'nhis', 'nhis_2021.zip' )
-# )
-
-
-df.2021 <- td_read_file(
-    file_path = file.path( '.', 'nhis', 'nhis_2021.zip' ),
-    read_func = read_csv,
-    specs = specs,
-
-    # Pick a 4-digit key from the file name for mapping names
-    mapping_key_pattern = '[0-9]{4}',
-
-    # Use rstadas function to add the survey year as metadata
-    metadata_func = td_metadata_year,
-
-    # Drop original (raw) data columns
-    keep_originals = FALSE,
-
-    # Additional arguments to the read_func (read_csv())
-    ## Read all as text
-    col_types = cols( .default = col_character() )
-)
-
-test_that(
-    'Data conforms to the specs', {
-        expect_named(
-            df.2021,
-            c(
-                'age', 'sex', 'height', 'weight', 'edulevel',
-                'nof_adults', 'nof_children', 'maritalstatus',
-                'health', 'lifesatisfaction',
-                'diff_walk100yd', 'diff_walk033miles', 'diff_walksteps',
-                'diff_communication', 'diff_remconc', 'diff_errands',
-                'diff_socactivities',
-                'mh_anxiety', 'mh_depression',
-                'k6_sadness', 'k6_nervous', 'k6_restless',
-                'k6_hopeless', 'k6_effort', 'k6_worthless'
-            )
-        )
-    }
-)
-
-
-df <- td_read_dir(
-    dir_path = file.path( '.', 'nhis' ),
-    specs = specs,
-    read_func = read_csv,
-    mapping_key_pattern ='[0-9]{4}',
-    metadata_func = td_metadata_year,
-    keep_originals = FALSE,
-
-    # Read all as text
-    col_types = cols( .default = col_character() )
-)
-
-test_that(
-    'Data conforms to the specs', {
-        expect_named(
-            df,
-            c(
-                'age', 'sex', 'height', 'weight', 'edulevel',
-                'nof_adults', 'nof_children', 'maritalstatus',
-                'health', 'lifesatisfaction',
-                'diff_walk100yd', 'diff_walk033miles', 'diff_walksteps',
-                'diff_communication', 'diff_remconc', 'diff_errands',
-                'diff_socactivities',
-                'mh_anxiety', 'mh_depression',
-                'k6_sadness', 'k6_nervous', 'k6_restless',
-                'k6_hopeless', 'k6_effort', 'k6_worthless'
-            )
-        )
-    }
-)
-
-
 
